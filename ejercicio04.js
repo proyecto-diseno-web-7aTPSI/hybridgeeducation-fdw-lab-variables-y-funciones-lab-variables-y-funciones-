ea6150513e78af871849f9d03f0c257b9ca64e1f
AlanAlonsoTPSI <66932563+AlanAlonsoTPSI@users.noreply.github.com>
2025-09-08 22:37:15 -0600

    Update ejercicio04.js

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,7 +1,8 @@
 // Ejercicio 4: Función que Saluda
 // Declara una función llamada "saludar" que acepte un parámetro "nombre" y retorne un mensaje de saludo con la estructura "Hola, nombre!".
 
-// Escribe tu función aquí. El nombre exacto debe ser saludar
+def saludar(Alan):
+    retunr f"Hola, {Alan}!""
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
