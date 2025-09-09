da387af74bafb14b059c93a16276edd02bc55c82
AlanAlonsoTPSI <66932563+AlanAlonsoTPSI@users.noreply.github.com>
2025-09-08 22:34:30 -0600

    Update ejercicio02.js

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,7 +1,11 @@
 // Ejercicio 2: Uso de Constantes en Funciones
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
-//define tu función aquí
+function obtenerPI(){
+    const PI = 3.1416;
+        return PI;
+    
+}
 
 
 // No modificar
