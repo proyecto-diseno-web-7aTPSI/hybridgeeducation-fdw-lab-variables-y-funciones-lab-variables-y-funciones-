e93a27083109eebfadb02d0a76f241b953e32841
AlanAlonsoTPSI <66932563+AlanAlonsoTPSI@users.noreply.github.com>
2025-09-08 22:32:26 -0600

    Update ejercicio01.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,7 +2,8 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    let nombre = "Alan";
+    return nombre;
 }
 
 // No modificar.
